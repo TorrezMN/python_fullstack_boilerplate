2b99793037288696e80a89cfdad0a81f37b7541f
TorrezMN <torrez.mn@gmail.com>
2023-06-27 17:15:17 -0400

    FINISH build config for react.

--- front/src/App.js
+++ front/src/App.js
@@ -9,6 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <center>
+          <h3>Pagina de mierda!</h3>
+          <h3>Pagina de mierda!</h3>
+          <h3>Pagina de mierda!</h3>
+          <h3>Pagina de mierda!</h3>
+        </center>
         <a
           className="App-link"
           href="https://reactjs.org"
